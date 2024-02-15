6baab59eb99e313991e10ac70c91f8b8c88ff651
⠀suki ⠀ <suk.ktaa@gmaiul.com>
2024-02-14 22:54:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function somaValor(){
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value); 
+    let soma = n1 + n2;
+
+    alert(soma);
+}
+function subValor(){
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value); 
+    let sub = n1 - n2;
+
+    alert(sub);
+}
+function multiValor(){
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value); 
+    let multi = n1 * n2;
+
+    alert(multi);
+}
+function divValor(){
+    let n1 = parseInt(document.getElementById('n1').value);
+    let n2 = parseInt(document.getElementById('n2').value); 
+    let div = n1 / n2;
+
+    alert(div);
+}
